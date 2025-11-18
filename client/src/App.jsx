5df746cb9912f89f753436ecc50b22325199b166
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-11-18 16:31:15 +0200

    Add game creation page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
 import Catalog from './components/catalog/Catalog.jsx';
 import Details from './components/details/Details.jsx';
+import GameCreate from './components/game-create/GameCreate.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<Catalog />} />
         <Route path="/games/:gameId/details" element={<Details />} />
+        <Route path="/games/create" element={<GameCreate />} />
       </Routes>
 
       <Footer />
