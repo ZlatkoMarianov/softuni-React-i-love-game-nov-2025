19f9b5c5801d7c79b18aedaae32738b12b9aa366
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-11-26 17:11:05 +0200

    Add navigation after editing game and update API URL

--- client/src/components/edit/Edit.jsx
+++ client/src/components/edit/Edit.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import request from '../../utils/request.js';
 
 const initialValues = {
@@ -12,6 +12,7 @@ const initialValues = {
 };
 
 export default function Edit() {
+  const navigate = useNavigate();
   const { gameId } = useParams();
   const [values, setValues] = useState(initialValues);
 
@@ -27,7 +28,7 @@ export default function Edit() {
   useEffect(() => {
     if (!gameId) return;
 
-    request(`http://localhost:3030/jsonstore/games/${gameId}`)
+    request(`/games/${gameId}`)
       .then((result) => {
         if (result) {
           setValues(result);
@@ -39,9 +40,20 @@ export default function Edit() {
       });
   }, [gameId]);
 
+  const editGameHandler = async () => {
+    try {
+      await request(`/games/${gameId}`, 'PUT', values);
+
+      navigate(`/games/${gameId}/details`);
+    } catch (err) {
+      alert(err.message);
+    }
+
+  };
+
   return (
     <section id="edit-page">
-      <form id="add-new-game">
+      <form id="add-new-game" action={editGameHandler}>
         <div className="container">
           <h1>Edit Game</h1>
           <div className="form-group-half">
